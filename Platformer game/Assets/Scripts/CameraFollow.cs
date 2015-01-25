778a5a65997f4be75385d7a3890b7b5c1e8302d2
ajinkyafaytown <ajinkyafaytown@consortya.com>
2015-01-25 13:55:19 -0800

    project added

---
+++ Platformer game/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		//transform.position = Vector3.Lerp (transform.position, target.position, Time.deltaTime);
+
+	}
+}
---
+++ Platformer game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float turnSmoothing = 15f;
+	public float speedDampTime = 0.1f;
+	private Animator anim;
+	private float speed = 1f;
+
+
+	void Awake ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate ()
+	{
+		//Keyboard and xbox controller controls.
+		float h = Input.GetAxis("Horizontal");
+		//float v = Input.GetAxis ("Vertical");
+
+		//float x = h * Time.deltaTime;// * speed;
+		//float y = v * Time.deltaTime * speed;
+
+		//transform.Translate(0, 0 , x);
+
+		if (h != 0f) {
+			Rotate (h);
+		}
+		anim.SetFloat("speed", Mathf.Abs(h));
+	}
+
+	void Rotate (float horizontal)
+	{
+		Vector3 targetDirection = new Vector3(horizontal, 0f, 0f);
+		Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+		Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+		rigidbody.MoveRotation(newRotation);
+	}	
+}
