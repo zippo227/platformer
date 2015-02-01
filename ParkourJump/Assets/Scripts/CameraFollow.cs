290f10226c75203c25853204e015eefc13ecd352
ajinkyafaytown <ajinkyafaytown@consortya.com>
2015-02-01 10:42:48 -0800

    updating player controls

--- ParkourJump/Assets/Scripts/CameraFollow.cs
+++ ParkourJump/Assets/Scripts/CameraFollow.cs
@@ -5,15 +5,16 @@ public class CameraFollow : MonoBehaviour {
 	public Transform target;
 	public float lerpSpeed = 3f;
 	public float xScreenOffset = 2f;
+	public float yScreenOffset = 2f;
 
 	private Vector3 velocity = new Vector3 (.5f, .5f, .5f);
 
 	void LateUpdate () {
 
 		float smoothX = Mathf.SmoothDamp (transform.position.x, target.position.x+xScreenOffset, ref velocity.x, .3f);
-		//float smoothY = Mathf.SmoothDamp (transform.position.y, target.position.y, ref velocity.y, .3f);
+		float smoothY = Mathf.SmoothDamp (transform.position.y, target.position.y+yScreenOffset, ref velocity.y, .3f);
 
-		transform.position = Vector3.Lerp (transform.position, new Vector3(smoothX, transform.position.y, transform.position.z), Time.deltaTime * lerpSpeed);
+		transform.position = Vector3.Lerp (transform.position, new Vector3(smoothX, smoothY, transform.position.z), Time.deltaTime * lerpSpeed);
 
 	}
 }
